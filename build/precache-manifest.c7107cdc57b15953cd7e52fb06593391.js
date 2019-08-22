66b9f207ac27ee815861f8909706ba788cc61f26
Ikenna Igwe <ikennaigwe@gmail.com>
2019-08-21 22:59:13 -0700

    Gin Scrummy
    Initial gin scrummy

---
+++ build/precache-manifest.c7107cdc57b15953cd7e52fb06593391.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6668572011e801d486501d9588cec634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0878fe8cbfc72fbf7c89",
+    "url": "/static/js/main.ff1553ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,1 @@
+// Start of Gin Scrummy App
