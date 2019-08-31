2f51d039ddbce8f28e7c5487f2961fbd0ac2dffc
Ikenna Igwe <ikennaigwe@gmail.com>
2019-08-30 23:31:32 -0700

    commit

---
+++ build/precache-manifest.8b6681348ee50a45fd24f1c57974f198.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c549b959f528e5bb9a9b23f64d65d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9574685677e6ddc9a61",
+    "url": "/static/css/main.32a6406d.chunk.css"
+  },
+  {
+    "revision": "eb68e7faaf43bd4d69b9",
+    "url": "/static/js/2.116219da.chunk.js"
+  },
+  {
+    "revision": "e9574685677e6ddc9a61",
+    "url": "/static/js/main.6c9215b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8723777ffe7a091252bb183705d249bd",
+    "url": "/static/media/scrummy-0.8723777f.svg"
+  },
+  {
+    "revision": "879d122a30566d2c20865554bb963ec6",
+    "url": "/static/media/scrummy-1-01.879d122a.svg"
+  },
+  {
+    "revision": "17289f1e5b306e3d94920429f3090305",
+    "url": "/static/media/scrummy-1-2.17289f1e.svg"
+  },
+  {
+    "revision": "23c7be8849bb20025b817ec2d5971567",
+    "url": "/static/media/scrummy-100.23c7be88.svg"
+  },
+  {
+    "revision": "44028d33c8fa64808e44c135d8539c09",
+    "url": "/static/media/scrummy-13-01.44028d33.svg"
+  },
+  {
+    "revision": "9a4fc1626e39ae25b2021bed2195d6ca",
+    "url": "/static/media/scrummy-144-01.9a4fc162.svg"
+  },
+  {
+    "revision": "b914bcb591125cb28b5351c498811428",
+    "url": "/static/media/scrummy-2-01.b914bcb5.svg"
+  },
+  {
+    "revision": "99ce8a147cd0753691e0f085b78683ca",
+    "url": "/static/media/scrummy-20-01.99ce8a14.svg"
+  },
+  {
+    "revision": "59edff2096de0888b611845ddb4066cf",
+    "url": "/static/media/scrummy-21-01.59edff20.svg"
+  },
+  {
+    "revision": "c62331cfb5f531910d88ba6b71816e6b",
+    "url": "/static/media/scrummy-3-01.c62331cf.svg"
+  },
+  {
+    "revision": "c51bd75f0a5767fc9c024297ba0a72a2",
+    "url": "/static/media/scrummy-34-01.c51bd75f.svg"
+  },
+  {
+    "revision": "7b2fe8ef419d7580a698a73a8a8ea984",
+    "url": "/static/media/scrummy-40-01.7b2fe8ef.svg"
+  },
+  {
+    "revision": "74420b6a34ffed5b5af51cb396d582c8",
+    "url": "/static/media/scrummy-5-01.74420b6a.svg"
+  },
+  {
+    "revision": "5d7fe2b5926e70e3c1287d1f80eb5125",
+    "url": "/static/media/scrummy-55-01.5d7fe2b5.svg"
+  },
+  {
+    "revision": "df958274b91570c9ff763bfe338e53b1",
+    "url": "/static/media/scrummy-8-01.df958274.svg"
+  },
+  {
+    "revision": "94d06d786014d05d87cbf4e6356682c3",
+    "url": "/static/media/scrummy-89-01.94d06d78.svg"
+  },
+  {
+    "revision": "bf96b8ea43cf3da20cf1147fb55d2bfb",
+    "url": "/static/media/scrummy-90-01.bf96b8ea.svg"
+  },
+  {
+    "revision": "bc94df10469b5e9f8fc4b61e5f5c54ab",
+    "url": "/static/media/scrummy-infinite-01.bc94df10.svg"
+  },
+  {
+    "revision": "0c2cde4b5f301af9f3a29031a0d16d6d",
+    "url": "/static/media/scrummy-question-01.0c2cde4b.svg"
+  },
+  {
+    "revision": "df85c2072cef120b28a9a8c2d26585a1",
+    "url": "/static/media/scrummy-teacup-01.df85c207.svg"
+  }
+]);
